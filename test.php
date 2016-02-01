7bf19e87d8d6efe7fff5e0eca5381cc6a5c53da4
xeArts <xearts@Takabayashi-no-MacBook-Pro.local>
2016-02-01 18:50:43 +0900

    phpのテスト

--- test.php
+++ test.php
@@ -1,7 +1,46 @@
 <?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
 
-for($i=1; $i<=100; $i++){
-    echo $i,PHP_EOL;
+class Nabeatsu
+{
+    /**
+     * @var int
+     */ 
+    private $maxNum;
+    
+    /**
+     * コンストラクタ
+     * 
+     * @param int $maxNum 
+     */
+    public function __construct($maxNum)
+    {
+        $this->maxNum = $maxNum;
+    }
+    
+    
+    /**
+     * カウントアップ
+     */ 
+    public function countUp(){
+        for ($i = 1; $i <= $this->maxNum; $i ++) {
+            echo $i;
+     
+            if ($i % 3 == 0) {
+                echo '!';
+            }
+           echo PHP_EOL;
+        }
+    }
 }
+
+$nabeatsu1 = new Nabeatsu(10);
+$nabeatsu2 = new Nabeatsu(20);
+$nabeatsu3 = new Nabeatsu(30);
+
+$nabeatsu1->countUp();
+$nabeatsu2->countUp();
+$nabeatsu3->countUp();
 
 ?>
